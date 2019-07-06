08425e01c2fbef65c2b9a3d7b669c6c875f7d094
Alexander Kuvaev <alexander@kuvaev.me>
2019-07-07 01:40:22 +0300

    Статистика, разбор сырых данных и генерация экстраполяционных коэффициентов

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 import numpy as np
 import statsmodels.api as sm
 import sklearn.linear_model as lm
+import sys
+import json
 
-x = [[2894, 14318], [2214, 14644], [2180, 14157], [2037, 23081], [2032, 21020], [2575, 21580]]
-y = [1287, 884, 747, 968, 976, 1174]
+d = {}
 
-x_ = sm.add_constant(x)
-smm = sm.OLS(y, x_)
-res = smm.fit()
-print(res.params)
-d = 600
-s = 25000
-print(res.params[0] + res.params[1]*d + res.params[2]*s)
+f = open('rawdata.txt', 'r')
 
-x1 = [[1287, 14318], [884, 14644], [747, 14157], [968, 23081], [976, 21020], [1174, 21580]]
-y1 = [2894, 2214, 2180, 2037, 2032, 2575]
+for line in f:
+    l = line.split()
+    k = " ".join(l[:-3])
+    try:
+        p1 = int(l[-3])
+        p2 = int(l[-2])
+        p3 = int(l[-1])
+        if not (k in d):
+            d[k] = []
+        d[k].append(([p1, p3], p2))
+    except Exception:
+        print(l)
 
-x_ = sm.add_constant(x1)
-smm = sm.OLS(y1, x_)
-res = smm.fit()
-print(res.params)
-d = 652
-s = 30812
-print(res.params[0] + res.params[1]*d + res.params[2]*s)
-
-
-x = [[1, 2894], [2, 2214], [3, 2180], [4, 2037], [5, 2032]]
-y = [1287, 884, 747, 800, 750]
-
-x_ = sm.add_constant(x)
-smm = sm.OLS(y, x_)
-res = smm.fit()
-print(res.params)
-for i in range(1, 21):
-    print(res.params[0] + res.params[1]*i + res.params[2]*2000)
-
-
+coeffs = {}
+for k in d:
+    if len(d[k]) < 3:
+        continue
+    x = []
+    y = []
+    for t1, t2 in d[k]:
+        x.append(t1)
+        y.append(t2)
+        x_ = sm.add_constant(x)
+        smm = sm.OLS(y, x_)
+        res = smm.fit()
+        coeffs[k] = json.dumps(list(map(float, res.params)))
+p = json.dumps(coeffs, ensure_ascii=False)
+f = open('data.json', 'w')
+print(p, file=f)
