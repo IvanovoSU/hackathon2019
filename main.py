fc955b6fb86393f80ef89035e1b0d88abcf48261
Alexander Kuvaev <alexander@kuvaev.me>
2019-07-07 08:09:57 +0300

    Добавил показатели необходимые для визуализации: общий тренд, количесвто ожидаемых свыпускников и количество ожидаемых трудоустроенных по каждому из 3 регионов по каждой специальности

--- main.py
+++ main.py
@@ -3,23 +3,53 @@ import statsmodels.api as sm
 import sklearn.linear_model as lm
 import sys
 import json
+import math
+
+regions = ['ivanovo', 'vladimir', 'yaroslavl']
 
 d = {}
+regd = {}
+salary = {}
+trend = {}
+student = {}
+def add_data(f, region):
+    regd[region] = {}
+    for line in f:
+        l = line.split()
+        k = " ".join(l[:-3])
+        try:
+            p1 = int(l[-3])
+            p2 = int(l[-2])
+            p3 = int(l[-1])
+            if not (k in d):
+                d[k] = []
+            if not (k in regd[region]):
+                regd[region][k] = []
+            d[k].append(([p2, p3], p1))
+            regd[region][k].append((p1, p2, p3))
+        except Exception:
+            print(l)
 
-f = open('rawdata.txt', 'r')
+def get_trend(region, param, trend_data):
+    trend_data[region] = {}
+    for k in regd[region]:
+        if len(regd[region][k]) < 2:
+            continue
+        x = []
+        y = []
+        q = 1
+        for t in regd[region][k]:
+            x.append(q)
+            q += 1
+            y.append(t[param])
+        x_ = sm.add_constant(x)
+        smm = sm.OLS(y, x_)
+        res = smm.fit()
+        trend_data[region][k] = math.ceil(res.params[0] + 4*res.params[1])
 
-for line in f:
-    l = line.split()
-    k = " ".join(l[:-3])
-    try:
-        p1 = int(l[-3])
-        p2 = int(l[-2])
-        p3 = int(l[-1])
-        if not (k in d):
-            d[k] = []
-        d[k].append(([p2, p3], p1))
-    except Exception:
-        print(l)
+for r in regions:
+    f = open('rawdata_' + r + '.txt', 'r')
+    add_data(f, r)
 
 coeffs = {}
 for k in d:
@@ -33,7 +63,26 @@ for k in d:
     x_ = sm.add_constant(x)
     smm = sm.OLS(y, x_)
     res = smm.fit()
-    coeffs[k] = json.dumps(list(map(float, res.params)))
+    coeffs[k] = list(res.params)
 p = json.dumps(coeffs, ensure_ascii=False)
-f = open('data.json', 'w')
+f = open('js/data.json', 'w')
 print(p, file=f)
+f = open('js/salary_data.json', 'w')
+trend_data = {}
+for r in regions:
+    get_trend(r, 2, trend_data)
+p = json.dumps(trend_data, ensure_ascii=False)
+print(p, file=f)
+f = open('js/trend.json', 'w')
+trend_data = {}
+for r in regions:
+    get_trend(r, 1, trend_data)
+p = json.dumps(trend_data, ensure_ascii=False)
+print(p, file=f)
+f = open('js/student.json', 'w')
+trend_data = {}
+for r in regions:
+    get_trend(r, 0, trend_data)
+p = json.dumps(trend_data, ensure_ascii=False)
+print(p, file=f)
+
